7790e30fdd11f7428916b3d1b00e61710da14105
LaraCosta66 <larasmila64@gmail.com>
2022-02-10 14:14:50 -0300

    Update exercises

--- Modulo D/aula012b/script.js
+++ Modulo D/aula012b/script.js
@@ -1,10 +1,10 @@
 function verificar() {
 
-   var data = new Date()
-    var ano = data.getFullYear()
+   var data = new Date() //ano atual
+    var ano = data.getFullYear() //Ano completo
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-
+    
     if(fano.value.length == 0 || fano.value > ano){
         window.alert('[erro] Verifique se os dados estão corretos')
     } else{
@@ -13,42 +13,40 @@ function verificar() {
         var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                //CRIANÇA
+               img.setAttribute('src', 'bebemenino.png')
+            } else if (idade < 21){
+                //JOVEM
+                img.setAttribute('src', 'jovemhomem.png')
+            } else if (idade < 50){
+                //Adulto
+              img.setAttribute('src', 'adultohomem.png')
 
-    }
-    /* if(fsex[0].checked){
-        genero = 'Homem'
-        if(idade >=0 && idade <10){
-            //Crianca
-            img.setAttribute('src','imagens/bebemenino.png') 
-        }
-        } else if(idade < 21){
-            //Jovem
-            img.setAttribute('src','imagens/jovemhomem.png') 
-        }else if (idade < 50){
-            //Adulto
-            img.setAttribute('src','imagens/adultohomem.png')
-        }else {
-            //Idioso
-            img.setAttribute('src', 'imagens/idosohomem.png')
-        } if(fsex[1].checked){
-            genero = 'Mulher'
-             }if(idade >=0 && idade <10){
-             //Crianca
-             img.setAttribute('src','imagens/bebe.png')
-            } else if(idade < 21){
-            //Jovem
-            img.setAttribute('src','imagens/jovemulher.png')
-
-             }else if (idade < 50){
-             //Adulto
-             img.setAttribute('src','imagens/adultomulher.png')
-
-            }else {
-             //Idioso
-             img.setAttribute('src','imagens/Idosamulher.png')
-
+            } else{
+                //IDOSO
+                img.setAttribute('src', 'idosohomem.png')
             }
-            res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-            res.appendChild(img)` */
-}  
-//Atualizar e verificar ainda está dando erro.
+        }else if (fsex[1].checked){
+            genero = 'Mulher' 
+            if(idade >= 0 && idade < 10){
+                //CRIANÇA
+                img.setAttribute('src', 'bebe.png')
+            } else if (idade < 21){
+                //JOVEM
+                img.setAttribute('src', 'jovemulher.png')
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'adultomulher.png')
+            } else{
+                //IDOSO
+                img.setAttribute('src', 'Idosamulher.png')
+            }
+        }
+        res.style.textAlign = 'Center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
---
+++ Modulo E/aula013/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do{//Estrutura de Repetiçao com teste lógico no final
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+} while( c <= 6)
+
+
+/*
+var c = 1 
+while( c <= 6){ ESTRUTURA DE REPETICAO COM TESTE LÓGICO NO INICIO
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+}
+
+*/
+
+/* console.log('ola, tudo bem ?')
+console.log('ola, tudo bem ?')
+console.log('ola, tudo bem ?')
+console.log('ola, tudo bem ?')
+console.log('ola, tudo bem ?')
+console.log('ola, tudo bem ?')*/
